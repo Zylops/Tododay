3c6ce07ba43a477b2f4aa2ea801b3c1d46f93278
Zylops <zainaazeez787@gmail.com>
2022-06-14 19:36:08 +0300

    Responsiveness Update

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
+<!-- Styling -->
+
 <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap');
-
 .font-poppins {
     font-family: 'Poppins', sans-serif;
 }
 </style>
-
 <script>
     tailwind.config = {
       theme: {
@@ -20,10 +20,18 @@
     }
 </script>
 
+<!-- Responsiveness -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Database Connection -->
+
 <?php
     include('config/dbConnect.php');
     session_start();
 ?>
+
+<!-- Header Banner -->
 
 <div class="p-16 flex justify-center align-middle bg-blue-200">
     <?php include('src/logo.php'); ?>
